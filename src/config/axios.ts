0e93596efe4b152164d16859f7d60d8151997cee
ahmadvu123-cmyk <ahmad.vu123@gmail.com>
2026-05-06 16:35:58 +0500

    Next js project setup

---
+++ src/config/axios.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    timeout: 10000,
+})
+api.interceptors.request.use((config) => {
+    // Used for Request Modification, Attaching Tokens, Add Headers Runs Before every request is send
+    return config;
+})
+
+api.interceptors.response.use((response) => response,
+(error) => {
+    console.log('API Error', error);
+    return Promise.reject(error);
+})
+
+export default api
