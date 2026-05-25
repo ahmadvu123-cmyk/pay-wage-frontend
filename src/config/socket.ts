44f85dcb250ca3350e6f5798da8bb9dcda6093f7
ahmadvu123-cmyk <ahmad.vu123@gmail.com>
2026-05-25 20:14:28 +0500

    final changes

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXT_PUBLIC_API_URL!, {
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
   transports: ["websocket"],
+  secure: true
 });
